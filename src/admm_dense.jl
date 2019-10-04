13dfcf060bb9184e824b1c6249b963706730e6fb
nrontsis <nrontsis@gmail.com>
2019-10-04 17:28:19 +0100

    first commit

---
+++ src/admm_dense.jl
@@ -0,0 +1,161 @@
+using Printf
+
+mutable struct Data{T}
+    n::Int
+    c::Vector{T}
+    x::Vector{T}
+    x_::Vector{T}
+    z::Vector{T} # This not really used except as a temporary variable
+    z_::Vector{T}
+    w::Vector{T}
+    y::Vector{T}
+    sigma::T
+    rho::T
+    alpha::T
+    iteration::Int
+    ε_abs::T
+    ε_rel::T
+    time::T
+    is_symmetric::Bool
+
+    function Data(C::AbstractArray{T}; rho=10, sigma=50, alpha=1.7, ε_abs=1e-4, ε_rel=0.0) where T
+        c = reshape(C, length(C))
+        n = Int(sqrt(length(c)))
+        z = ones(2*n); z_ = ones(2*n);
+        w = zeros(n^2); y = zeros(2*n)
+        C = reshape(c, n, n)
+        X = (C + abs.(C))./2
+        new{T}(n, c, X[:], X[:], z, z_, w, y, sigma, rho, alpha, 0, ε_abs, ε_rel, 0.0, issymmetric(C))
+    end
+end
+
+function print_info(data::Data)
+    if mod(data.iteration, 500) == 0 || data.iteration == 0
+        @printf("Iter \t  Objective \t Primal Res \t Dual Res \t Time (s) \n")
+    end
+    X = reshape(data.x, data.n, data.n)
+    Ax = A(data.x)
+    r_prim = norm(Ax .- 1, Inf)
+    r_dual = norm(data.x - data.c + At(data.y) + data.w, Inf)
+
+    @printf("%d \t  %.5e \t %.5e \t %.5e \t %.5e\n",
+        data.iteration,
+        norm(data.x)^2/2 - dot(data.x, data.c),
+        r_prim,
+        r_dual,
+        data.time
+    )
+
+    ε_prim = data.ε_abs + data.ε_rel*max(norm(Ax, Inf), 1)
+    @inbounds y1 = view(data.y, 1:data.n)
+    @inbounds y2 = view(data.y, data.n+1:2*data.n)
+    ε_dual = data.ε_abs + data.ε_rel*max(norm(data.x, Inf), norm(y1, Inf)*norm(y2, Inf), norm(data.c, Inf), norm(data.w, Inf))
+    if r_prim <= ε_prim && r_dual <= ε_dual
+        done = true
+    else
+        done = false
+    end
+
+    δw = data.sigma*(data.x_ - data.x)
+    δy = data.rho*(data.alpha*data.z_ .- data.alpha)
+    nzindices = data.c .!= 0
+    δw = δw[nzindices]
+    if (norm(δw + At(δy)[nzindices], Inf) <= data.ε_abs) && sum(δy) + sum(δw + abs.(δw))/2 <= -data.ε_abs
+        println("The problem is infeasible.")
+        println("Certificate:")
+        println("||δw + At(δy)||: ", norm(δw + At(δy)[nzindices], Inf))
+        println("sum(δy) + sum(δw_+): ", sum(δy) + sum(δw + abs.(δw))/2)
+        done = true
+    end
+    return done
+end
+
+function admm(C; max_iterations=5000, kwargs...)
+    data = Data(C; kwargs...)
+    done = print_info(data)
+    while !done && data.iteration !== max_iterations
+        data.time += @elapsed iterate!(data)
+        if mod(data.iteration, 50) == 0
+            done = print_info(data)
+        end
+    end
+    return data
+end
+
+function iterate!(data::Data)
+    solve_linear_system!(data)
+    @. data.x_ = data.alpha*data.x_ + (1 - data.alpha)*data.x
+    @. data.x = data.x_ + data.w/data.sigma
+    project!(data)
+    @. data.w += data.sigma*(data.x_ - data.x)
+    @. data.y += data.rho*(data.alpha*data.z_ - data.alpha)
+    data.iteration += 1
+end
+
+function project!(data::Data)
+    @inbounds for i = 1:length(data.x)
+        if data.c[i] == 0 || data.x[i] <= 0
+            data.x[i] = 0
+        elseif data.x[i] >= 1
+            data.x[i] = 1
+        end
+    end
+end
+
+function solve_linear_system!(data::Data)
+    data.z .= data.rho .- data.y # Here data.z is used as a temporary variable
+    data.z_ .= 0
+    @inbounds for j = 1:data.n, i = 1:data.n
+            idx = (j - 1)*data.n + i
+            data.x_[idx] = data.sigma*data.x[idx] - data.w[idx] + data.z[i] + data.z[j + data.n] + data.c[idx]
+            # Equivalent to doing data.z_ .= A(data.x_) after the loop
+            # data.z_[j + data.n] += data.x_[idx]
+            # data.z_[i] += data.x_[idx]
+    end
+    data.z_ .= A(data.x_, data.is_symmetric)
+    solve_reduced_linear_system!(data.z_, data.rho, data.sigma)
+    @inbounds for j = 1:data.n, i = 1:data.n
+            idx = (j - 1)*data.n + i
+            data.x_[idx] = (data.x_[idx] - data.rho*data.z_[i] - data.rho*data.z_[j + data.n])/(1 + data.sigma)
+    end
+end
+
+function A(x, is_symmetric=false)
+    n = Int(sqrt(length(x)))
+    X = reshape(x, n, n)
+    sum_rows = reshape(sum(X, dims=1), n)
+    if is_symmetric
+        sum_cols = sum_rows
+    else
+        sum_cols = reshape(sum(X, dims=2), n)
+    end
+    return [sum_cols; sum_rows]
+end
+
+function At(z)
+    n = div(length(z), 2)
+    z1 = view(z, 1:n)
+    z2 = view(z, n+1:2*n)
+    return reshape(z2' .+ z1, n^2)
+end
+
+function solve_reduced_linear_system!(x, rho, sigma)
+    n = div(length(x), 2)
+    c1 = 1/(1 + n*rho + sigma)
+    c2 = c1^2*rho^2*n/(1 - c1^2*rho^2*n^2)
+    c3 = -c1*rho*(1 + n*c2)
+
+    @inbounds x1 = view(x, 1:n)
+    @inbounds x2 = view(x, n+1:2*n)
+    # s1 and s2 are always equal as they are the sum of the colum-sums and sum of the row-sums of a matrix
+    s1 = sum(x1); s2 = sum(x2)
+
+    lmul!(c1, x)
+    c = c1*(c2*s1 + c3*s2)
+    x1 .+= c
+    c = c1*(c3*s1 + c2*s2)
+    x2 .+= c
+    # Both of the c above are equal, due to the same argument as for s1 and s2
+
+    return x
+end
---
+++ src/admm_sparse.jl
@@ -0,0 +1,223 @@
+using Printf
+using LinearAlgebra, SparseArrays, SuiteSparse
+using AMD
+
+function triangulize!(X)
+    n = size(X, 1)
+    @inbounds for j in 1:n, idx in X.colptr[j]:X.colptr[j+1]-1
+        i = X.rowval[idx]
+        if j == i
+            X.nzval[idx] /= 2
+        elseif j < i
+            X.nzval[idx] =0
+        end
+    end
+    return dropzeros!(X)
+end
+
+triangulize(X) = triangulize!(copy(X))
+
+mutable struct Data{T, Ti}
+    n::Int
+    C::SparseMatrixCSC{T, Ti}
+    F::SuiteSparse.CHOLMOD.Factor{T} # F::SparseMatrixCSC{T, Ti}
+    H::Vector{T}
+    x::Vector{T}
+    x_::Vector{T}
+    z::Vector{T} # This not really used except as a temporary variable
+    z_::Vector{T}
+    w::Vector{T}
+    y::Vector{T}
+    sigma::T
+    rho::T
+    alpha::T
+    iteration::Int
+    ε_abs::T
+    ε_rel::T
+    time::T
+    print_interval::Ti
+    primal_residuals::Vector{T}
+    dual_residuals::Vector{T}
+    update_rho::Bool
+
+    function Data(C::SparseMatrixCSC{T, Ti}; rho=50.0, sigma=1.0, alpha=1.7, ε_abs=1e-4, ε_rel=0.0, print_interval=50) where {T, Ti}
+        Cu = triangulize!(copy(C))
+        n = size(Cu, 1)
+        z = ones(n); z_ = ones(n);
+        w = zeros(nnz(Cu)); y = zeros(n)
+        x = copy(Cu.nzval)
+        H = generate_cost(Cu).diag
+        Cu.nzval .*= H
+
+        new{T, Ti}(n, Cu, cholesky!(sprandn(0, 0, 0.0)), H, 0*x, 0*copy(x), z, z_, w, y, sigma, rho, alpha, 0, ε_abs, ε_rel, 0.0,
+            print_interval, zeros(T, 0), zeros(T, 0), true)
+    end
+end
+
+function compute_factorization!(data::Data)
+    S = copy(data.C); S.nzval .= 1
+    S = (S + S')
+    S.nzval ./= (2 + data.sigma)
+    @inbounds for i in 1:size(S, 1)
+        S[i, i] *= (2 + data.sigma)
+        S[i, i] /= (4 + data.sigma)
+    end
+    S .*= data.rho
+    sums = sum(S, dims=1)
+    S = S + I
+    @inbounds for i in 1:size(S, 1)
+        S[i, i] += sums[i]
+    end
+    print("Computing cholesky...")
+    data.F = cholesky!(S)
+    println("  Done!")
+end
+
+function print_info(data::Data)
+    if mod(data.iteration, data.print_interval*10) == 0 || data.iteration == 0
+        @printf("Iter \t  Objective \t Primal Res \t Dual Res \t Time (s) \n")
+    end
+    Ax = mul_A(data.C, data.x)
+    r_prim = norm(Ax .- 1, Inf)
+    r_dual = norm(data.H.*data.x - data.C.nzval + mul_At(data.C, data.y) + data.w, Inf)
+
+    @printf("%d \t  %.5e \t %.5e \t %.5e \t %.5e\n",
+        data.iteration,
+        data.x'*(data.H.*data.x)/2 - dot(data.x, data.C.nzval),
+        r_prim,
+        r_dual,
+        data.time
+    )
+
+    if r_prim <= data.ε_abs && r_dual <= data.ε_abs
+        done = true
+    else
+        done = false
+    end
+
+    ratio = r_prim/r_dual
+    if ratio > 20 && !done && data.update_rho
+        # set_phase!(data.ps, Pardiso.RELEASE_ALL)
+        # pardiso(data.ps)
+        # data.ps = MKLPardisoSolver()
+        data.sigma *= 10; data.rho *= 10
+        compute_factorization!(data)
+    end
+    if ratio < 1/20 && !done && data.update_rho
+        # set_phase!(data.ps, Pardiso.RELEASE_ALL)
+        # pardiso(data.ps)
+        # data.ps = MKLPardisoSolver()
+        data.sigma /= 10; data.rho /= 10
+        compute_factorization!(data)
+    end
+    
+    return done
+end
+
+function admm_inner(data, max_iterations)
+    done = print_info(data)
+    while !done && data.iteration !== max_iterations
+        data.time += @elapsed iterate!(data)
+        if mod(data.iteration, data.print_interval) == 0
+            done = print_info(data)
+        end
+        #=
+        Ax = mul_A(data.C, data.x)
+        r_prim = norm(Ax .- 1, Inf)
+        r_dual = norm(data.H.*data.x - data.C.nzval + mul_At(data.C, data.y) + data.w, Inf)
+        append!(data.primal_residuals, r_prim)
+        append!(data.dual_residuals, r_dual)
+        =#
+    end
+    return data
+end
+
+function admm(C::SparseMatrixCSC; max_iterations=5000, kwargs...)
+    data = Data(C; kwargs...)
+    compute_factorization!(data)
+    admm_inner(data, max_iterations)
+    return data
+end
+
+function admm(C::SparseMatrixCSC, data::Data; max_iterations=5000)
+    data.iteration = 0
+    data.C = triangulize!(copy(C))
+    H = generate_cost(data.C).diag
+    data.C.nzval .*= H
+
+    compute_factorization!(data)
+    data.update_rho = false
+    admm_inner(data, max_iterations)
+    return data
+end
+
+function iterate!(data::Data)
+    solve_linear_system!(data)
+    @. data.x_ = data.alpha*data.x_ + (1 - data.alpha)*data.x
+    @. data.x = data.x_ + data.w/data.sigma
+    project!(data)
+    @. data.w += data.sigma*(data.x_ - data.x)
+    @. data.y += data.rho*(data.alpha*data.z_ - data.alpha)
+    data.iteration += 1
+end
+
+function project!(data::Data)
+    @inbounds for i = 1:length(data.x)
+        if data.x[i] <= 0
+            data.x[i] = 0
+        elseif data.x[i] >= 1
+            data.x[i] = 1
+        end
+    end
+end
+
+function solve_linear_system!(data::Data)
+    data.z .= data.rho .- data.y # Here data.z is used as a temporary variable
+    data.z_ .= 0
+    # Change to sparse representation
+    @inbounds for j in 1:data.n
+        for idx in data.C.colptr[j]:data.C.colptr[j+1]-1
+            i = data.C.rowval[idx]
+            data.x_[idx] = data.sigma*data.x[idx] - data.w[idx] + data.z[i] + data.z[j] + data.C.nzval[idx]
+            # Equivalent to doing data.z_ .= A(data.x_) after the loop
+            # data.z_[j] += data.x_[idx]
+            # data.z_[i] += data.x_[idx]
+        end
+    end
+    data.z .= mul_A(data.C, data.x_./(data.H .+ data.sigma))
+    # set_phase!(data.ps, 33) # Solve, iterative refinement
+    # pardiso(data.ps, data.z_, data.F, data.z)
+    data.z_ .= data.F\data.z
+    # Change to sparse representation
+    @inbounds for j in 1:data.n
+        for idx in data.C.colptr[j]:data.C.colptr[j+1]-1
+            i = data.C.rowval[idx]
+            data.x_[idx] = data.x_[idx] - data.rho*data.z_[i] - data.rho*data.z_[j] # ToDo: inverse
+        end
+    end
+    data.x_ ./= (data.H .+ data.sigma)
+end
+
+function mul_A(C::SparseMatrixCSC, x)
+    n = size(C, 1)
+
+    y = zeros(n)
+    @inbounds for j in 1:n, idx in C.colptr[j]:C.colptr[j+1]-1
+        i = C.rowval[idx]
+        y[i] += x[idx]
+        y[j] += x[idx]
+    end
+
+    return y
+end
+
+function mul_At(C::SparseMatrixCSC, x)
+    n = size(C, 1)
+    y = zeros(nnz(C))
+    @inbounds for j in 1:n, idx in C.colptr[j]:C.colptr[j+1]-1
+        i = C.rowval[idx]
+        y[idx] += x[i] + x[j]
+    end
+
+    return y
+end
