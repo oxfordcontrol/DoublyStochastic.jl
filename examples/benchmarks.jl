b09978651021da3968c5db0adb7ebe9028838106
nrontsis <nrontsis@gmail.com>
2019-10-05 15:40:29 +0100

    Benchmark script runs for all categories :tada:

--- examples/benchmarks.jl
+++ examples/benchmarks.jl
@@ -5,44 +5,50 @@ include("../src/admm_dense.jl")
 include("load_problems.jl")
 include("../src/other_solvers.jl")
 
+normalize_data = false
+problem_set = :ocean # Other options :suitesparse or :random
+
+parameters_set_suitespace = 1:100
+parameters_set_ocean = [(h, d) for d in 10.0^-1.1:-0.1:-1.3 for h in pi./[8; 16; 32; 64]][:]
+parameters_set_random = [(n, 0.001) for n in (100, 1000, 10000)]
+parameters_set = Dict(zip((:suitesparse, :ocean, :random),
+    (parameters_set_suitespace, parameters_set_ocean, parameters_set_random)))
+
 df = DataFrame(name = String[], n = Int[], nnz = Int[],
     time = Float64[], time_gurobi = Float64[], 
     iter = Int[]
 )
-normalize_data = true
-problem_set = :random # Other options :suitesparse or :random
 
-for h in pi./[8; 16; 32; 64]
-    for i in 0.1:0.1:0.3 # 0:0.1:3
-        if problem_set == :suitesparse
-            C, name = load_suitesparse(i)
-        elseif problem_set == :ocean
-            C, name = load_ocean(h, 10.0^(-1 - i))
-        elseif problem_set == :random
-            name = "random"
-            n = Int(floor(i*100))
-            C = sprandn(n, n, 1.0); C = (C + C')/2;
-        end
-
-        # Scaling
-        @printf("Problem Statistics: min value: %.3f, max: %.3f, nnz: %d, rows: %d.\n",
-            minimum(C.nzval), maximum(C.nzval), nnz(C), size(C, 1))
-        if normalize_data
-            @info "Prescaling matrix data to [0, 1] with nonzero diagonal."
-            C = abs.(C)
-            if maximum(C.nzval) > 1
-                C = C./maximum(C)
-            end
-            C = C + 1e-16*I
-        end
-
-        t_gurobi = 0.0; t = 0.0; iter = 0
-        # t_gurobi = @elapsed X_gurobi = doubly_stochastic_gurobi(C)
-        t = @elapsed X, data = solve(C, max_iterations=20000, sigma=5, rho=50, alpha=1.6, ε_abs=1e-4)
-        # @show norm(X - X_gurobi)
-            
-        push!(df, [name, size(C, 1), nnz(C), t, t_gurobi, iter])
-        df |> CSV.write(string("../results/", problem_set, ".csv"))
-        @assert false
+for parameter in parameters_set[problem_set]
+    if problem_set == :suitesparse
+        C, name = load_suitesparse(parameter)
+    elseif problem_set == :ocean
+        h = parameter[1]; d = parameter[2]
+        C, name = load_ocean(h, d)
+    elseif problem_set == :random
+        name = "random"
+        n = parameter[1]; sparsity = parameter[2]
+        @show n
+        C = sprandn(n, n, sparsity); C = (C + C')/2;
     end
+
+    # Scaling
+    @printf("Problem Statistics: min value: %.3f, max: %.3f, nnz: %d, rows: %d.\n",
+        minimum(C.nzval), maximum(C.nzval), nnz(C), size(C, 1))
+    if normalize_data
+        @info "Prescaling matrix data to [0, 1] with nonzero diagonal."
+        C = abs.(C)
+        if maximum(C.nzval) > 1
+            C = C./maximum(C)
+        end
+        C = C + 1e-16*I
+    end
+
+    t_gurobi = 0.0; t = 0.0; iter = 0
+    # t_gurobi = @elapsed X_gurobi = doubly_stochastic_gurobi(C)
+    t = @elapsed X, data = solve(C, max_iterations=20000, sigma=5, rho=50, alpha=1.6, ε_abs=1e-4)
+    # @show norm(X - X_gurobi)
+        
+    push!(df, [name, size(C, 1), nnz(C), t, t_gurobi, iter])
+    df |> CSV.write(string("../results/", problem_set, ".csv"))
 end
--- examples/load_problems.jl
+++ examples/load_problems.jl
@@ -5,7 +5,7 @@ using Printf
 
 function load_suitesparse(i)
     working_dir = pwd()
-    path = "./WeightedGraphs/"
+    path = "data/WeightedGraphs/"
     cd(path)
     files = glob("*.mat")
     cd(working_dir)
@@ -30,25 +30,33 @@ function load_spambase(sigma=20.0)
     return C, "spambase"
 end
 
-function load_ocean(h=pi/8, d=1e-3)
-    #=
+function generate_ocean_file(h_max=pi/8)
     N = 128*256
     x = rand(N, 2)
     x[:, 1] = 2*pi*x[:, 1]
     x[:, 2] = 4*pi*x[:, 2] .- pi
     D = pairwise(Euclidean(), x, dims=1)
     D .+= 1e-20
-    D[D .> pi/8] .= 0
+    D[D .> h_max] .= 0
     D = SparseMatrixCSC(D)
-    @save "ocean.jld2" D
-    =#
-    @load "data/Ocean/ocean.jld2"
-    @assert h <= pi/8
+    @save "data/Ocean/ocean.jld2" D h_max
+    return D
+end
+
+function load_ocean(h=pi/8, d=1e-3)
+    # Generating the data can take a lot of time and memroy
+    # Check if the data has already been generated, if so try to load them
+    if isfile("data/Ocean/ocean.jld2")
+        @load "data/Ocean/ocean.jld2"
+        if h > h_max
+            D, h_max = generate_ocean_file(max(h, pi/8))
+        end
+    else
+        D, _ = generate_ocean_file(max(h, pi/8))
+    end
 
     D.nzval[D.nzval .> h] .= 0
     dropzeros!(D)
     D.nzval .= exp.(-D.nzval./(4*d*0.1))
-    @show h
-    @printf("%.2E", h)
     return D, string("ocean_h", @sprintf("%.2E", h), "_d", @sprintf("%.2E", d))
 end
